dc0a7ba6f81671a643cef002feb47629c1450be6
Crosas27 <c.rosas27@icloud.com>
2026-04-11 21:05:52 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,39 @@
-const CACHE = 'panelpro-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'metal-panel-v12';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './js/colors.js',
+  './js/measurement.js',
+  './js/engine.js',
+  './js/numpad.js',
+  './js/components.js',
+  './js/walls.js',
+  './js/app.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600;700&display=swap',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
